56887d89869f37162a0627ae86cd5aa17b40d104
Ted M. Young <tedyoung@gmail.com>
2021-07-26 16:20:15 -0700

    Lab #6: Stand redirects to /done and player is done

--- src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackControllerTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackControllerTest.java
@@ -99,6 +99,21 @@ class BlackjackControllerTest {
                 .isNotBlank();
     }
 
+    @Test
+    public void playerStandsResultsInGamePlayerDone() throws Exception {
+        Game game = new Game(new Deck());
+        BlackjackController blackjackController = new BlackjackController(game);
+        blackjackController.startGame();
+
+        String redirectPage = blackjackController.standCommand();
+
+        assertThat(redirectPage)
+                .isEqualTo("redirect:/done");
+
+        assertThat(game.isPlayerDone())
+                .isTrue();
+    }
+
 }
 
 
