c3cc27cf5810a28d017cbc315ad5e722269841d2
Ted M. Young <tedyoung@gmail.com>
2021-07-26 14:55:11 -0700

    GET of /game returns GameView rendered through the blackjack.html template.

--- src/test/java/com/jitterted/ebp/blackjack/domain/StubDeck.java
+++ src/test/java/com/jitterted/ebp/blackjack/domain/StubDeck.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 
-class StubDeck extends Deck {
+public class StubDeck extends Deck {
     private static final Suit DUMMY_SUIT = Suit.HEARTS;
     private final ListIterator<Card> iterator;
 
@@ -13,6 +13,10 @@ class StubDeck extends Deck {
         for (Rank rank : ranks) {
             cards.add(new Card(DUMMY_SUIT, rank));
         }
+        this.iterator = cards.listIterator();
+    }
+
+    public StubDeck(List<Card> cards) {
         this.iterator = cards.listIterator();
     }
 
