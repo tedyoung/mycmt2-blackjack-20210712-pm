eac347bfd042d8969188fb8b6f542666ce61a980
Ted M. Young <tedyoung@gmail.com>
2021-07-26 16:24:40 -0700

    Lab #6: Dealer draws card when player stands

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
@@ -50,6 +50,8 @@ public class BlackjackController {
     @PostMapping("/stand")
     public String standCommand() {
         game.playerStands();
+        game.dealerTurn();
+
         return "redirect:/done";
     }
 
